8074bc0823838747454635730c6db264e23ee7f1
Jason <jason.melton2@gmail.com>
2025-06-30 22:25:01 -0500

    adjust todo styles

--- app/components/TodoCard.tsx
+++ app/components/TodoCard.tsx
@@ -7,7 +7,7 @@ export const TodoCard = (props: { todo: ToDoItem }) => {
   // console.log("props", props);
   const { name, description } = props.todo;
   return (
-    <div className="card px-4 py-2">
+    <div className="card px-2 pt-2 pb-4 mx-[-1px] mb-[-1px]">
       <div className="flex items-start justify-between">
         <h2 className="h3 p-0 mb-2">{name}</h2>
         <CloseButton onClick={() => {}} />
